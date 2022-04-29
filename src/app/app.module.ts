bf82b3cd4f1228f0974c270a2fee40d1631454df
Thi <thi@ideta.io>
2022-04-29 21:29:21 +0200

    add child

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ChildComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
